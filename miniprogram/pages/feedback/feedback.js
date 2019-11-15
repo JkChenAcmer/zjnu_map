e1fba641a2d91e00dcaf7de0037a00708f1d1ea6
JkChenAcmer <47021484+JkChenAcmer@users.noreply.github.com>
2019-11-15 16:16:55 +0800

    add feedback function

--- miniprogram/pages/feedback/feedback.js
+++ miniprogram/pages/feedback/feedback.js
@@ -2,11 +2,14 @@ const app = getApp()
 Page({
   data: {
     context: '',
+    From: '',
     empty: 1
   },
 
   formSubmit: function (e) {
     var that = this;
+    console.log(this.data.From);
+    console.log(this.data.context);
     if (this.data.empty == 1) {     //如果文本框内容为空，则跳出提示
       wx: wx.showToast({
         title: '内容不能为空!',
@@ -21,6 +24,34 @@ Page({
         content: '是否提交你的反馈意见？',
         success(res) {
           if (res.confirm) {
+            wx.cloud.callFunction({
+              name:'send_email',
+              data:{
+                From:that.data.From,
+                Context:that.data.context,
+                Email:'1834131427@qq.com',
+              },
+              complete: console.log('send successed')
+            })
+            wx.cloud.callFunction({
+              name: 'send_email',
+              data: {
+                From: that.data.From,
+                Context: that.data.context,
+                Email: '1249552715@qq.com',
+              },
+              complete: console.log('send successed')
+            })
+            wx.cloud.callFunction({
+              name: 'send_email',
+              data: {
+                From: that.data.From,
+                Context: that.data.context,
+                Email: '1172685677@qq.com',
+              },
+              complete: console.log('send successed')
+            })
+
             wx: wx.showToast({
               title: '提交成功!',
               icon: true,
@@ -62,6 +93,12 @@ Page({
         console.log("res: " + that.data.empty)
       }
     }, 300)
+  },
+  // 字符变动
+  charChange2: function (e) {
+    this.setData({
+      From: e.detail.value
+    })
   }
 
 })
